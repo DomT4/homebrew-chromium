f3e39a6017c81156c641c8b152d9ff28622f2921
Dominyk Tiller <dominyktiller@gmail.com>
2014-10-15 04:08:45 +0100

    Chromium 299624
    Latest upstream Chromium. This version is POODLE-resistant.

--- chromium.rb
+++ chromium.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Chromium < Formula
    homepage "https://www.chromium.org/"
-   url "https://raw.githubusercontent.com/DomT4/LibreMirror/master/Chromium/Mac_299253_chrome-mac.zip"
-   version "299253"
-   sha256 "3eb6224cec4c31de020c458a9e3009f17fdf84a2c08ddf6f72815c2a153da049"
+   url "https://raw.githubusercontent.com/DomT4/LibreMirror/master/Chromium/Mac_299624_chrome-mac.zip"
+   version "299624"
+   sha256 "f917a317ab391a875a16c55c9f9e947ea9ae5f3bc135b053624c0ecdb7218575"
 
   # Why are we using your mirror instead of the core download link?
   # Answer - Because the core download link isn't wget or curl friendly.
