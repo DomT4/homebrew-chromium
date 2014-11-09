37f6c955a5b476e021d41261ce19f12378d4bea0
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-09 17:45:51 +0000

    chromium 303403
    Version bump.

--- chromium.rb
+++ chromium.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Chromium < Formula
    homepage "https://www.chromium.org/"
-   url "https://raw.githubusercontent.com/DomT4/LibreMirror/master/Chromium/Mac_303050_chrome-mac.zip"
-   version "303050"
-   sha256 "63da067cca5f792d4c56203319a92ec2ffde6ddfbcf9e94dbbfbb0cbf5dc8cac"
+   url "https://raw.githubusercontent.com/DomT4/LibreMirror/master/Chromium/Mac_303403_chrome-mac.zip"
+   version "303403"
+   sha256 "ff79ef0da87302152a0f733dbcfd238bd0b25f6d7e705539262c992308bf2b7e"
 
   # Why are we using your mirror instead of the core download link?
   # Answer - Because the core download link isn't wget or curl friendly.
