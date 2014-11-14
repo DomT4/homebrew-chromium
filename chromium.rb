910917d8543e617945ad3393b629fbc73ff33917
Dominyk Tiller <dominyktiller@gmail.com>
2014-11-14 02:48:03 +0000

    chromium 304131
    version bump

--- chromium.rb
+++ chromium.rb
@@ -2,12 +2,13 @@ require "formula"
 
 class Chromium < Formula
    homepage "https://www.chromium.org/"
-   url "https://raw.githubusercontent.com/DomT4/LibreMirror/master/Chromium/Mac_303403_chrome-mac.zip"
-   version "303403"
-   sha256 "ff79ef0da87302152a0f733dbcfd238bd0b25f6d7e705539262c992308bf2b7e"
+   url "https://raw.githubusercontent.com/DomT4/LibreMirror/master/Chromium/Mac_304131_chrome-mac.zip"
+   version "304131"
+   sha256 "c3344ceee3991636064b56f625e67deecdfe3e437a36998d533214204d69c5bd"
 
   # Why are we using your mirror instead of the core download link?
   # Answer - Because the core download link isn't wget or curl friendly.
+  # Please feel free to PR if you have a better way of handling that.
 
   def install
     prefix.install "Chromium.app"
