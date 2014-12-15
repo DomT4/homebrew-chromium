afa2a8664d0102997a25a7c05453b10c90b530c6
Dominyk Tiller <dominyktiller@gmail.com>
2014-12-15 05:56:23 +0000

    chromium c308312
    Commit/Version bump.

--- chromium.rb
+++ chromium.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Chromium < Formula
    homepage "https://www.chromium.org/getting-involved/download-chromium"
-   url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F307649%2Fchrome-mac.zip?generation=1418195183117000&alt=media"
-   version "c307649"
-   sha256 "c2b5361188b9ed671883be10ce62e09674881d79639577240241581be0db2399"
+   url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F308312%2Fchrome-mac.zip?generation=1418621041784000&alt=media"
+   version "c308312"
+   sha256 "da009e36c1153b153e2686c901724217dfcf136a6b9bc0e11eb6abd0f9a129c9"
 
   def install
     prefix.install "Chromium.app"
