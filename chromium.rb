33ed5ebf0cc962b85cee299a36c554f328667097
Dominyk Tiller <dominyktiller@gmail.com>
2015-01-22 01:11:25 +0000

    chromium c312494
    Commit Bump.
    
    I usually don’t bump twice in the same day, but the last bump contained
    some relatively annoying glitches that made the fan speed run up
    considerably, so bumping again with the hope that was fixed upstream.
    Small cost of using an otherwise brilliant dev snapshot.

--- chromium.rb
+++ chromium.rb
@@ -1,8 +1,8 @@
 class Chromium < Formula
    homepage "https://www.chromium.org/getting-involved/download-chromium"
-   url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F312320%2Fchrome-mac.zip?generation=1421847228396000&alt=media"
-   version "c312320"
-   sha256 "2a78774c26d2a45f36946f3a490f1250dd928bc75479b5a0574e3fe759fafefd"
+   url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F312494%2Fchrome-mac.zip?generation=1421887304160000&alt=media"
+   version "c312494"
+   sha256 "6be2f9287460fee7ee15b153665c8d14b08a1fa8b6757eb6449aea3928cb117e"
 
   def install
     prefix.install "Chromium.app"
