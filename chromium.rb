3a48ab91979929e8436c8f3f400c0bb25431745e
Dominyk Tiller <dominyktiller@gmail.com>
2015-01-23 00:20:39 +0000

    chromium: added optional exec script
    Changes:
    
    1) Style nits. Fixes up the rogue indentation.
    2) Moves the Application from Prefix to Libexec; This prevents Homebrew
    from removing any empty directories inside the App. It wasn’t causing
    problems, but it’s not a good thing to do to an App.
    3) Adds an optional exec script for super-easy CLI opening of the App.

--- chromium.rb
+++ chromium.rb
@@ -1,10 +1,21 @@
 class Chromium < Formula
-   homepage "https://www.chromium.org/getting-involved/download-chromium"
-   url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F312675%2Fchrome-mac.zip?generation=1421962006063000&alt=media"
-   version "c312675"
-   sha256 "85ab120d94087243e731087f74b1569bb81fa24a2acbde71871ef20fd29a70ef"
+  homepage "https://www.chromium.org/getting-involved/download-chromium"
+  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F312675%2Fchrome-mac.zip?generation=1421962006063000&alt=media"
+  version "c312675"
+  sha256 "85ab120d94087243e731087f74b1569bb81fa24a2acbde71871ef20fd29a70ef"
+
+  option "with-exec-script", "Installs an exec script for easy CLI opening of Chromium"
 
   def install
-    prefix.install "Chromium.app"
+    libexec.install "Chromium.app"
+
+    if build.with? "exec-script"
+      exec_script = bin+"chromium"
+      exec_script.write <<-EOS.undent
+        #!/usr/bin/env bash
+        exec open -a "Chromium" "$@"
+      EOS
+      exec_script.chmod 0755
+    end
   end
 end
