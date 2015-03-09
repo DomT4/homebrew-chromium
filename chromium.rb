b8b6630368523a27f77e376326b2d7be91a1b155
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-09 02:49:01 +0000

    chromium c319597
    Commit bump.

--- chromium.rb
+++ chromium.rb
@@ -1,8 +1,8 @@
 class Chromium < Formula
   homepage "https://www.chromium.org/getting-involved/download-chromium"
-  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F318996%2Fchrome-mac.zip?generation=1425436024748000&alt=media"
-  version "c318996"
-  sha256 "6b7911a06a040efd20f83ff8124ee7676fc4b0ee17017a3517ecddf5b631d780"
+  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F319597%2Fchrome-mac.zip?generation=1425844711372000&alt=media"
+  version "c319597"
+  sha256 "e69fa03c6037a54524915b2dc91f7953bcf284602b21dd502da42bdb06a6d674"
 
   option "with-exec-script", "Installs an exec script for easy CLI opening of Chromium"
 
