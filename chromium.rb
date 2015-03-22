296817add310fc2132d17211b4e9c44e39bb3023
Dominyk Tiller <dominyktiller@gmail.com>
2015-03-22 18:26:40 +0000

    chromium c321718
    Bump.

--- chromium.rb
+++ chromium.rb
@@ -1,8 +1,8 @@
 class Chromium < Formula
   homepage "https://www.chromium.org/getting-involved/download-chromium"
-  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F320859%2Fchrome-mac.zip?generation=1426568047791000&alt=media"
-  version "c320859"
-  sha256 "bc60100e843e48772e2c14ea2e0bdbf90ee4cbee2277fc51ed0d08f5c88b4197"
+  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F321718%2Fchrome-mac.zip?generation=1427046331157000&alt=media"
+  version "c321718"
+  sha256 "af1646cc79107365df95f002d62d7913ba6f9febbbb6eb7ff0780ed8cbc1cd88"
 
   option "with-exec-script", "Installs an exec script for easy CLI opening of Chromium"
 
