2d0475bd7032d3a3bbb648a415128e244dd3ba33
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-03 00:48:14 +0100

    chromium c337262

--- chromium.rb
+++ chromium.rb
@@ -1,9 +1,9 @@
 class Chromium < Formula
   desc "An open-source web browser"
   homepage "https://www.chromium.org/getting-involved/download-chromium"
-  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F335457%2Fchrome-mac.zip?generation=1434917601889000&alt=media"
-  version "c335457"
-  sha256 "691480709ec76e416c5e5c66ab213da4b0b532f287cc3793f25f1ed03fc792a1"
+  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F337262%2Fchrome-mac.zip?generation=1435879229914000&alt=media"
+  version "c337262"
+  sha256 "d51e6735e1baa50140628f7b2b3e105bbf2632abdebd4fee121820e162739e82"
 
   option "with-exec-script", "Installs an exec script for easy CLI opening of Chromium"
 
@@ -11,7 +11,7 @@ class Chromium < Formula
     libexec.install "Chromium.app"
 
     if build.with? "exec-script"
-      exec_script = bin+"chromium"
+      exec_script = bin/"chromium"
       exec_script.write <<-EOS.undent
         #!/usr/bin/env bash
         exec open -a "Chromium" "$@"
