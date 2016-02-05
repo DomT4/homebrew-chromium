b60d5f399adbdd127cb7e5e85aa12b27ed001b3a
Dominyk Tiller <dominyktiller@gmail.com>
2016-02-05 20:44:34 +0000

    chromium c373796

--- chromium.rb
+++ chromium.rb
@@ -1,9 +1,9 @@
 class Chromium < Formula
   desc "Open-source web browser"
   homepage "https://www.chromium.org/getting-involved/download-chromium"
-  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F372345%2Fchrome-mac.zip?generation=1454083044930000&alt=media"
-  version "c372345"
-  sha256 "763bf7e9344df631a12e55377de33b4867721db135b1bc4220791e7ca7841a4a"
+  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F373796%2Fchrome-mac.zip?generation=1454677553290000&alt=media"
+  version "c373796"
+  sha256 "27d6a3ffcef92794da1578b75a5c0bec5e4f1048119394e3d0511548caf4301f"
 
   option "with-exec-script", "Installs an exec script for easy CLI opening of Chromium"
 
