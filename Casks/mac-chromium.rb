c39a25200f80de3c691feb8cc0816816f175eaed
Dominyk Tiller <dominyktiller@gmail.com>
2017-02-12 13:40:08 +0000

    Casks/mac-chromium c449890

--- Casks/mac-chromium.rb
+++ Casks/mac-chromium.rb
@@ -1,8 +1,8 @@
 cask "mac-chromium" do
-  version "c446947"
-  sha256 "31fc0f6bcd47abc90fc210e4457d85851698448b0bdb141dd605245e97b9de4c"
+  version "c449890"
+  sha256 "f043d44b7f45e2df994f67c964818ccb0417503293134d6577682b207fe16f18"
 
-  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F446947%2Fchrome-mac.zip?generation=1485727646101493&alt=media"
+  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F449890%2Fchrome-mac.zip?generation=1486902277930811&alt=media"
   name "Mac-Chromium"
   homepage "https://www.chromium.org/"
   conflicts_with cask: "chromium"
