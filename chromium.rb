9874f1b733b80ad6bc20b473cdca0acc9522a01a
Dominyk Tiller <dominyktiller@gmail.com>
2017-03-12 16:32:45 +0000

    chromium c456300

--- chromium.rb
+++ chromium.rb
@@ -1,9 +1,9 @@
 class Chromium < Formula
   desc "Open-source web browser"
   homepage "https://www.chromium.org/getting-involved/download-chromium"
-  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F453039%2Fchrome-mac.zip?generation=1487988453911595&alt=media"
-  version "c453039"
-  sha256 "4974201011f0a99e8584cee683a6e54df960dce3ad76e2ba5308bb021cde9873"
+  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F456300%2Fchrome-mac.zip?generation=1489315559960629&alt=media"
+  version "c456300"
+  sha256 "c18e3ca324c480cb4b899e871d03b37c2a8fae779646f4a54df7185371196e99"
 
   option "with-exec-script", "Install a script for easy CLI opening of Chromium"
 
@@ -25,7 +25,7 @@ class Chromium < Formula
     towards killing the usefulness and support of installing .apps through
     Homebrew formulae completely. I strongly disagree with removing app
     functionality through Homebrew formulae but the decision is out of my
-    hands these days, sorry.
+    hands, sorry.
 
     You can work around this once linkapps stops functioning by doing:
       cp -rf #{opt_libexec}/Chromium.app /Applications
