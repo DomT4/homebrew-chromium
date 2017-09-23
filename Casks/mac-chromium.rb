9c6a145d2b44ba192f3b1be95d064c7890e780ae
Dominyk Tiller <dominyktiller@gmail.com>
2017-09-23 23:03:37 +0100

    Casks/mac-chromium c503945

--- Casks/mac-chromium.rb
+++ Casks/mac-chromium.rb
@@ -1,8 +1,8 @@
 cask "mac-chromium" do
-  version "c502082"
-  sha256 "9c17a2c9e9008b9eb9119cf5e2969583aecc2f046dc585e9101d91db7aae649e"
+  version "c503945"
+  sha256 "e30a48b7020db34efb11f8de4e423c05abdcdec02242d163a0aa18489ba149bb"
 
-  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F502082%2Fchrome-mac.zip?generation=1505433253993892&alt=media"
+  url "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Mac%2F503945%2Fchrome-mac.zip?generation=1506193445405306&alt=media"
   name "Mac-Chromium"
   homepage "https://www.chromium.org/"
   conflicts_with cask: "chromium"
